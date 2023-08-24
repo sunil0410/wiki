26fc9869b38eb3e7ad5f5d6e9d921be062a252fa
Ashutosh Tripathi <ashucoder9@gmail.com>
2023-08-24 14:24:17 +0530

    feat(wiki): fix broken link on landing page

--- src/data/features.js
+++ src/data/features.js
@@ -25,7 +25,7 @@ export const firstRow = [
     },
     {
         title: "Run a zkEVM node",
-        linkUrl: "docs/zkevm/setup-production-zknode",
+        linkUrl: "docs/zkevm/setup-production-node",
         imageUrl: "img/logo-round-dark.png",
         description: "Contribute to decentralized computing by running your own ZK node."
     },
