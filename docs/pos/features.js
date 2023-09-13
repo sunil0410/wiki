a0ed09c41320e80587fd920c14abd67590305a7e
DSalman03 <danny.f.salman@gmail.com>
2023-09-12 23:40:01 -0400

    enh welcome page with cards related to content + descriptions

--- docs/pos/features.js
+++ docs/pos/features.js
@@ -1,17 +1,32 @@
 export const firstRow = [
     {
         title: "What is Polygon PoS?",
-        linkUrl: "/docs/pos/getting-started",
+        linkUrl: "docs/pos/what-is-polygon-pos/",
         imageUrl: "/img/supernets/supernets-logo.png",
+        description: "Unlock the potential of Polygon's scalable and secure PoS architecture.",
     },
     {
         title: "Deploy a Contract",
         linkUrl: "/docs/category/deploy-a-contract",
         imageUrl: "/img/supernets/supernets-logo.png",
+        description: "Deploy smart contracts seamlessly on Polygon's high-performance network.",
     },
     {
         title: "Run a Node",
         linkUrl: "/docs/category/operate-a-node",
         imageUrl: "/img/supernets/supernets-logo.png",
+        description: "Empower your blockchain solutions by running a node on Polygon.",
+    },
+    {
+        title: "Become a Validator",
+        linkUrl: "/docs/category/become-a-validator/",
+        imageUrl: "/img/supernets/supernets-logo.png",
+        description: "Be a cornerstone of network security and earn rewards as a validator.",
+    },
+    {
+        title: "Become a Delegator",
+        linkUrl: "/docs/delegate/delegate/",
+        imageUrl: "/img/supernets/supernets-logo.png",
+        description: "Back network validators with MATIC and earn rewards as a delegator.",
     },
 ];
