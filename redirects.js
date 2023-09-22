6aa5e4470e4d66efbb3a1ceb90aa4e7698d5b11e
DSalman03 <danny.f.salman@gmail.com>
2023-09-22 12:51:42 -0400

    edits & fixs

--- redirects.js
+++ redirects.js
@@ -58,22 +58,6 @@ module.exports = [
       to: `/docs/tools/wallets/${doc}`,
       from: `/docs/develop/wallets/${doc}`,
     })),
-    /*
-    // Supernets Redirects
-    ...[
-        'operate', 'interfaces'
-    ].map(subpath => ({
-        to: `/docs/edge/${subpath}`,
-        from: `/docs/supernets/${subpath}`,
-    })),
-    */
-    // Wallets Redirects
-    ...[
-      'getting-started', 'polygon-web-wallet/web-wallet-v3-guide', 'adding-a-custom-token'
-    ].map(doc => ({
-      to: `/docs/tools/wallets/${doc}`,
-      from: `/docs/develop/wallets/${doc}`,
-    })),
     // Other Redirects
     {
       to: '/docs/pos/pos-faqs',
